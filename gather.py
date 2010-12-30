783accf37c2825d1aadd0d3622cbfc18a57b181d
Michael E Brown <mebrown@michaels-house.net>
2010-12-30 15:40:52 -0600

    start adding sql saving of file data

--- gather.py
+++ gather.py
@@ -15,6 +15,8 @@ import copy
 import shutil
 import fnmatch
 import logging
+import sqlobject
+import inspect
 from optparse import OptionGroup
 from trace_decorator import decorate, traceLog, getLog
 
@@ -39,6 +41,8 @@ def validate_args(opts, args):
     if opts.outputdir is None:
         raise basic_cli.CLIError("Output directory is required.")
 
+    opts.dbpath = os.path.join(opts.outputdir, "sqlite.db")
+
     opts.signoff = {}
     for csvfile in opts.signoff_fns:
         try:
@@ -55,6 +59,10 @@ def add_cli_options(parser):
     parser.add_option("-s", "--signoff-file", action="append", dest="signoff_fns", help="specify the signoff file", default=[])
     parser.add_option_group(group)
 
+    group = OptionGroup(parser, "General Options")
+    parser.add_option("--initdb", action="store_true", dest="initdb", help="Initialize storage Database", default=False)
+    parser.add_option_group(group)
+
     group = OptionGroup(parser, "Replace CMD defaults")
     group.add_option("--cmd-file", action="store", dest="cmd_file", help="specify file command", default="file")
     group.add_option("--cmd-find", action="store", dest="cmd_find", help="specify find command", default="find")
@@ -63,6 +71,125 @@ def add_cli_options(parser):
     group.add_option("--cmd-nm", action="store", dest="cmd_nm", help="specify nm command", default="nm")
     group.add_option("--cmd-scanelf", action="store", dest="cmd_scanelf", help="specify scanelf command", default="scanelf")
     parser.add_option_group(group)
+
+def check_prereqs(opts):
+    for cmd in [opts.cmd_find, opts.cmd_file, opts.cmd_rpm, opts.cmd_nm, opts.cmd_objdump, opts.cmd_scanelf]:
+        ret = redirect_call(['which', cmd], stdout_fn="/dev/null", stderr_fn="/dev/null", stdin_fn="/dev/null",)
+        if ret != 0:
+            raise PrereqError( "COULD NOT FIND PREREQUISITE: %s" % cmd )
+
+
+decorate(traceLog())
+def connect(opts):
+    if not os.path.exists(opts.dbpath):
+        opts.initdb = True
+
+    if opts.initdb:
+        if os.path.exists(opts.dbpath):
+            moduleLogVerbose.info("unlinking old db: %s" % opts.dbpath)
+            os.unlink(opts.dbpath)
+
+        if os.path.dirname(opts.dbpath) and not os.path.exists(os.path.dirname(opts.dbpath)):
+            os.makedirs(os.path.dirname(opts.dbpath))
+
+    moduleLogVerbose.info("Connecting to db at %s" % opts.dbpath)
+    sqlobject.sqlhub.processConnection = sqlobject.connectionForURI('sqlite://%s' % opts.dbpath)
+
+    if opts.initdb:
+        createTables()
+
+def gather_data(opts, dirpath, filename):
+    File(full_path=os.path.join(dirpath, filename), filename=filename)
+
+
+def main(): 
+    parser = basic_cli.get_basic_parser(usage=__doc__, version="%prog " + __VERSION__)
+    add_cli_options(parser)
+    opts, args = basic_cli.command_parse(parser, validate_fn=validate_args)
+    # DO NOT LOG BEFORE THIS CALL:
+    basic_cli.setupLogging(opts)
+
+    check_prereqs(opts)
+    connect(opts)
+
+    # Make Cache, gather data
+    if not os.path.exists(opts.outputdir):
+        os.makedirs(opts.outputdir)
+
+    for dirpath, dirnames, filenames in os.walk(opts.inputdir):
+        for filename in filenames:
+            outpath = os.path.join(opts.outputdir, dirpath)
+            if not os.path.exists(outpath):
+                os.makedirs(outpath)
+            gather_data(opts, dirpath, filename)
+
+    #print_license_report(opts)
+
+    # Print out collected error list global global_error_list
+    if len(global_error_list.values()):
+        sys.stderr.write("Here are all the problems I found:\n")
+        keys = global_error_list.keys()
+        keys.sort()
+        for err in keys:
+            sys.stderr.write(global_error_list[err] + "\n")
+
+
+# centralized place to set common sqlmeta class details
+class myMeta(sqlobject.sqlmeta):
+    lazyUpdate = False
+
+class File(sqlobject.SQLObject):
+    class sqlmeta(myMeta): pass
+    full_path = sqlobject.StringCol()
+    filename = sqlobject.StringCol()
+    tags = sqlobject.MultipleJoin('Tags')
+
+class Tags(sqlobject.SQLObject):
+    class sqlmeta(myMeta): pass
+    full_path = sqlobject.ForeignKey('File')
+    tag = sqlobject.StringCol()
+    info = sqlobject.StringCol()
+
+def createTables():
+    # fancy pants way to grab all classes in this file
+    # that are descendents of SQLObject and run .createTable() on them.
+    toCreate = [ value for key, value in globals().items()
+            if     inspect.isclass(value)
+               and value.__module__==__name__
+               and issubclass(value, sqlobject.SQLObject)
+         ]
+
+    for clas in toCreate:
+        clas.createTable(ifNotExists=True, createJoinTables=False)
+
+
+class CommentedFile:
+    def __init__(self, f, commentstring="#"):
+        self.f = f
+        self.commentstring = commentstring
+
+    def next(self):
+        line = self.f.next()
+        while (line[0] in self.commentstring) or line == "":
+            line = self.f.next()
+        return line
+
+    def __iter__(self):
+        return self
+
+
+def create_library_xref(csvdict, xref):
+    for line in csvdict:
+        try:
+            d = xref.get(line["LIBRARY"], {})
+            d[line["APPLICABLE"]] = line
+            xref[line["LIBRARY"]] = d
+        except Exception, e:
+            sys.stderr.write("="*79 + "\n")
+            sys.stderr.write("Ignoring parsing error in CSV file:")
+            traceback.print_exc()
+            sys.stderr.write("="*79 + "\n")
+    return xref
 
 def redirect_call(*args, **kwargs):
     close_stdout=0
@@ -94,75 +221,6 @@ def redirect_call(*args, **kwargs):
         kwargs["stdin"].close()
 
     return ret
-
-def check_prereqs(opts):
-    for cmd in [opts.cmd_find, opts.cmd_file, opts.cmd_rpm, opts.cmd_nm, opts.cmd_objdump, opts.cmd_scanelf]:
-        ret = redirect_call(['which', cmd], stdout_fn="/dev/null", stderr_fn="/dev/null", stdin_fn="/dev/null",)
-        if ret != 0:
-            raise PrereqError( "COULD NOT FIND PREREQUISITE: %s" % cmd )
-
-
-def main(): 
-    parser = basic_cli.get_basic_parser(usage=__doc__, version="%prog " + __VERSION__)
-    add_cli_options(parser)
-    opts, args = basic_cli.command_parse(parser, validate_fn=validate_args)
-    # do no logging calls before this line
-    basic_cli.setupLogging(opts)
-
-    check_prereqs(opts)
-
-    # Make Cache, gather data
-    if not os.path.exists(opts.outputdir):
-        os.makedirs(opts.outputdir)
-
-    for dirpath, dirnames, filenames in os.walk(opts.inputdir):
-        for filename in filenames:
-            outpath = os.path.join(opts.outputdir, dirpath)
-            if not os.path.exists(outpath):
-                os.makedirs(outpath)
-            #gather_data(opts, dirpath, filename)
-
-    #print_license_report(opts)
-
-    # Print out collected error list global global_error_list
-    if len(global_error_list.values()):
-        sys.stderr.write("Here are all the problems I found:\n")
-        keys = global_error_list.keys()
-        keys.sort()
-        for err in keys:
-            sys.stderr.write(global_error_list[err] + "\n")
-
-
-
-class CommentedFile:
-    def __init__(self, f, commentstring="#"):
-        self.f = f
-        self.commentstring = commentstring
-
-    def next(self):
-        line = self.f.next()
-        while (line[0] in self.commentstring) or line == "":
-            line = self.f.next()
-        return line
-
-    def __iter__(self):
-        return self
-
-
-def create_library_xref(csvdict, xref):
-    for line in csvdict:
-        try:
-            d = xref.get(line["LIBRARY"], {})
-            d[line["APPLICABLE"]] = line
-            xref[line["LIBRARY"]] = d
-        except Exception, e:
-            sys.stderr.write("="*79 + "\n")
-            sys.stderr.write("Ignoring parsing error in CSV file:")
-            traceback.print_exc()
-            sys.stderr.write("="*79 + "\n")
-    return xref
-
-
 
 
 if __name__ == "__main__":
