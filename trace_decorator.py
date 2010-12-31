7b62fb7586f27ffc808bea7338c9805ea1969b34
Michael E Brown <mebrown@michaels-house.net>
2010-12-30 21:07:46 -0600

    fixup tracing messages to have proper comma delimits

--- trace_decorator.py
+++ trace_decorator.py
@@ -83,12 +83,10 @@ def traceLog(log = None):
             if isinstance(l2, basestring):
                 l2 = logging.getLogger(l2)
 
-            message = "ENTER %s(" % func_name
-            for arg in args:
-                message = message + repr(arg) + ", "
-            for k,v in kw.items():
-                message = message + "%s=%s" % (k,repr(v))
-            message = message + ")"
+            message = "ENTER %s(" % func_name \
+                + ", ".join(repr(a) for a in args) + ", " \
+                + ", ".join( [ "%s=%s" % (k,repr(v)) for k,v in kw.items() ] )  \
+                + ")"
 
             frame = sys._getframe(2)
             doLog(l2, logging.INFO, os.path.normcase(frame.f_code.co_filename), frame.f_lineno, message, args=[], exc_info=None, func=frame.f_code.co_name)
