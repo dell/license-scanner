0f7fcd699ca03428ec47c68446f21511d04eb3a0
Michael E Brown <michael_e_brown@dell.com>
2011-01-12 12:35:15 -0600

    allow collapsing entries in list for html output

--- report.py
+++ report.py
@@ -36,6 +36,37 @@ global_error_list = {}
 moduleLog = getLog()
 moduleLogVerbose = getLog(prefix="verbose.")
 
+
+format_strings = { "html": {
+            "prefix": """\
+<html>
+    <head>
+        <title>Report</title>
+        <script type="text/javascript" src="mktree.js"></script>
+        <link rel="stylesheet" href="mktree.css" type="text/css">
+    </head>
+    <body>
+        <p>
+        <A href="#" onClick="expandTree('tree1'); return false;">Expand All</A>&nbsp;&nbsp;&nbsp;
+        <A href="#" onClick="collapseTree('tree1'); return false;">Collapse All</A>&nbsp;&nbsp;&nbsp;
+        </p>
+        <ul class="mktree" id="tree1">\
+""",
+            "suffix": "\t</ul>\n</body>",
+            "increase_level": "\t    {level}<ul>",
+            "decrease_level": "\t    {level}</ul>",
+            "end_item": "\t{opened}</li>",
+            "fmt_incompatible": "\t    {level}<li p='{levelno}'><font color='darkred'>{basename}    **(({license}))</font>",
+            "fmt_culprit":      "\t    {level}<li p='{levelno}'><font color='red'>{basename}    -->(({license}))</font>",
+            "fmt_ok":           "\t    {level}<li p='{levelno}'>{basename}    [{license}]"
+        },
+        "text": {
+            "fmt_incompatible": "{level}{basename}\t**(({license}))",
+            "fmt_culprit":      "{level}{basename}\t-->(({license}))",
+            "fmt_ok":           "{level}{basename}\t    [{license}]",
+            "post_per_exe": "",
+        }}
+
 class PrereqError(Exception): pass
 
 def validate_args(opts, args):
@@ -56,10 +87,6 @@ def add_cli_options(parser):
     parser.add_option("--html-output", action="store_const", const="html", dest="output_fmt", help="specify html output format")
     parser.add_option_group(group)
 
-    #group = OptionGroup(parser, "General Options")
-    #parser.add_option_group(group)
-
-
 decorate(traceLog())
 def connect(opts):
     moduleLogVerbose.info("Connecting to db at %s" % opts.dbpath)
@@ -74,27 +101,6 @@ def main():
 
     moduleLogVerbose.debug("Connecting to database.")
     connect(opts)
-
-    format_strings = { "html": {
-            "prefix": "<html><head></head><body><ol>",
-            "suffix": "</ol></body>",
-            "increase_level": "<ol>",
-            "decrease_level": "</ol>",
-            "fmt_incompatible": "<li><font color='darkred'>{basename}    **(({license}))</font></li>",
-            "fmt_culprit":      "<li><font color='red'>{basename}    -->(({license}))</font></li>",
-            "fmt_ok":           "<li>{basename}    [{license}]</li>"
-        },
-        "text": {
-            "prefix": None,
-            "suffix": None,
-            "increase_level": None,
-            "decrease_level": None,
-            "fmt_incompatible": "{level}{basename}\t**(({license}))",
-            "fmt_culprit":      "{level}{basename}\t-->(({license}))",
-            "fmt_ok":           "{level}{basename}\t    [{license}]",
-            "post_per_exe": "",
-        }}
-
     str_list = format_strings[opts.output_fmt]
 
     def log_if_not_empty(s, *args, **kargs):
@@ -105,20 +111,14 @@ def main():
     log_if_not_empty("prefix")
     for fname in license_db.Filedata.select():
         level = 0
+        opened = 0
         log_if_not_empty("pre_per_exe")
         for info in reversed(list(license_db.iter_over_dt_needed(opts, fname))):
             interpolate = {}
             interpolate["basename"] = info["filedata"].basename
             interpolate["license"]  = license_db.get_license(info["filedata"])
-            interpolate["level"]  = "    "*info["level"]
-
-            while info["level"] > level:
-                level = level + 1
-                log_if_not_empty("increase_level")
-
-            while info["level"] < level:
-                level = level - 1
-                log_if_not_empty("decrease_level")
+            interpolate["level"]  = "    " * info["level"]
+            interpolate["levelno"]  = info["level"]
 
             if not info["compatible"]:
                 fmtstring = "fmt_incompatible"
@@ -127,10 +127,40 @@ def main():
             else:
                 fmtstring = "fmt_ok"
 
+            while level < info["level"]:
+                interpolate["level"]  = "    " * level
+                level = level + 1
+                log_if_not_empty("increase_level", **interpolate)
+
+            while opened > info["level"]:
+                interpolate["opened"] = "    " * opened
+                log_if_not_empty("end_item", **interpolate)
+                opened = opened - 1
+                if level > info["level"]:
+                    interpolate["level"]  = "    " * (level-1)
+                    log_if_not_empty("decrease_level", **interpolate)
+                    level = level - 1
+
+            while level > info["level"]:
+                interpolate["level"]  = "    " * level
+                log_if_not_empty("decrease_level", **interpolate)
+                level = level - 1
+
+            interpolate["level"]  = "    " * info["level"]
+            opened = opened + 1
             log_if_not_empty(fmtstring, **interpolate)
+ 
         while level > 0:
+            interpolate["opened"] = "    " * opened
+            log_if_not_empty("end_item", **interpolate)
+            opened = opened - 1
+            interpolate["level"]  = "    " * level
+            log_if_not_empty("decrease_level", **interpolate)
             level = level - 1
-            log_if_not_empty("decrease_level")
+
+        interpolate["opened"] = "    " * opened
+        log_if_not_empty("end_item", **interpolate)
+            
         log_if_not_empty("post_per_exe")
     log_if_not_empty("suffix")
 
